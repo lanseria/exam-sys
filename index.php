7ba46129d4b4d3dc2e470c8d710b2acdfd0dc16d
Lanseria <zhangchao564265135@hotmail.com>
2017-03-09 13:11:42 +0800

    fix

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+define('APP_DEBUG',false);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
-
+error_reporting(0);
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
